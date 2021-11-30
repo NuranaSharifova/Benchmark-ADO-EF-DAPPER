5ac80eb4531b1e6a572cd49393603af3e67faaf4
Sari_nu95@itstep.edu.az <Sari_nu95@itstep.edu.az>
2021-11-30 17:29:59 +0400

    Add project files.

---
+++ Benchmark-ADO-EF-DAPPER/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Benchmark_ADO_EF_DAPPER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
